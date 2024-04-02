0f3d4962dfd559218c7d2b397e2e238e9b46adff
Alves0611 <g.alves0611@gmail.com>
2024-04-02 01:48:15 -0300

    added initial list of participants with basic information

---
+++ html-css-js/main.js
@@ -0,0 +1,63 @@
+let participants = [
+    {
+        name: "Diego Fernandes",
+        email: "diego@gmail.com",
+        registrationDate: new Date(2024, 2, 1, 19, 23),
+        dataCheckIn: new Date(2024, 2, 1, 20, 20)
+    },
+    {
+        name: "Mayk Brito",
+        email: "mayk@gmail.com",
+        registrationDate: new Date(2024, 2, 23, 19, 23),
+        dataCheckIn: new Date(2024, 2, 25, 20, 20)
+    },
+    {
+        name: "Ana Souza",
+        email: "ana@gmail.com",
+        registrationDate: new Date(2024, 0, 3, 19, 23),
+        dataCheckIn: new Date(2024, 0, 4, 20, 20)
+    },
+    {
+        name: "João Silva",
+        email: "joao@gmail.com",
+        registrationDate: new Date(2023, 11, 4, 19, 23),
+        dataCheckIn: new Date(2023, 11, 5, 20, 20)
+    },
+    {
+        name: "Maria Oliveira",
+        email: "maria@gmail.com",
+        registrationDate: new Date(2023, 10, 5, 19, 23),
+        dataCheckIn: new Date(2023, 10, 6, 20, 20)
+    },
+    {
+        name: "Pedro Santos",
+        email: "pedro@gmail.com",
+        registrationDate: new Date(2023, 9, 6, 19, 23),
+        dataCheckIn: new Date(2023, 9, 7, 20, 20)
+    },
+    {
+        name: "Carla Lima",
+        email: "carla@gmail.com",
+        registrationDate: new Date(2023, 8, 7, 19, 23),
+        dataCheckIn: new Date(2023, 8, 8, 20, 20)
+    },
+    {
+        name: "Lucas Sousa",
+        email: "lucas@gmail.com",
+        registrationDate: new Date(2023, 7, 8, 19, 23),
+        dataCheckIn: new Date(2023, 7, 9, 20, 20)
+    },
+    {
+        name: "Paula Costa",
+        email: "paula@gmail.com",
+        registrationDate: new Date(2023, 6, 9, 19, 23),
+        dataCheckIn: new Date(2023, 6, 10, 20, 20)
+    },
+    {
+        name: "Gabriel Almeida",
+        email: "gabriel@gmail.com",
+        registrationDate: new Date(2023, 5, 10, 19, 23),
+        dataCheckIn: new Date(2023, 5, 11, 20, 20)
+    }
+];
+
