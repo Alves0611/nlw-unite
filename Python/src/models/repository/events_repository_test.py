38c2b1049306bacc39046518d03a392ae90ceb79
Alves0611 <g.alves0611@gmail.com>
2024-04-03 11:33:31 -0300

    addin try in events repository

--- Python/src/models/repository/events_repository_test.py
+++ Python/src/models/repository/events_repository_test.py
@@ -18,11 +18,11 @@ def test_insert_event():
     response = events_repository.insert_event(event)
     print(response)
 
-@pytest.mark.skip(reason="It is not necessary")
+@pytest.mark.skip(reason="it is not necessary")
 def test_get_event_by_id():
     event_id = "my-uuid"
     events_repository = EventsRepository()
     response = events_repository.get_event_by_id(event_id)
     print(response)
-    print(response.title)
+    #print(response.title)
     
