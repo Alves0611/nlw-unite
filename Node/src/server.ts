d8174451de041aa80842e29178647ada3cbd50f6
Alves0611 <g.alves0611@gmail.com>
2024-04-04 09:50:45 -0300

    class 2 and 3 finished

--- Node/src/server.ts
+++ Node/src/server.ts
@@ -1,51 +1,53 @@
 import fastify from "fastify";
-import { z } from 'zod';
-import { PrismaClient } from '@prisma/client';
 
-const app = fastify()
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
-const prisma = new PrismaClient({
-    log: ['query'],
-});
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform, ZodTypeProvider } from 'fastify-type-provider-zod'
+import { createEvent } from "./routes/create-events";
+import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
+import { errorHandler } from "./error-handler";
 
-app.post('/events', async (request, reply) => {
-    const createEventSchema = z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable(),
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-    })
-
-    const {
-        title,
-        details,
-        maximumAttendees,
-    } = createEventSchema.parse(request.body)
-
-    const slug = generateSlug(title)
-
-    const eventWithSameSlug = await prisma.event.findUnique({
-        where: {
-            slug,
-        }
-    })
-
-    if (eventWithSameSlug !== null) {
-        throw new Error('Another event with same title already exists.')
-    }
-
-    const event = await prisma.event.create({
-        data: {
-            title,
-            details,
-            maximumAttendees,
-            slug,
-        },
-    })
-
-    return reply.status(201).send({ eventId: event.id })
+app.register(fastifyCors, {
+  origin: '*',
 })
 
-app.listen({ port: 3333 }).then(() => {
-    console.log('HTTP server running!')
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'pass.in',
+      description: 'Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.',
+      version: '1.0.0'
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent)
+app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
+app.register(checkIn)
+app.register(getEventAttendees)
+
+app.setErrorHandler(errorHandler)
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running!')
 })
